7d19e800f05d1bb3e72aa0ed839048c98dcd9d74
IshaiPeretz <ishaiperetz@yahoo.com>
2025-12-23 21:10:32 +0200

    backend basics

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://ishaiperetz_db_user:5Vq8WGAwKJm1i3U2@cluster0.lcca85h.mongodb.net/?appName=Cluster0',
+  dbName : process.env.DB_NAME || 'station_db'
+}
